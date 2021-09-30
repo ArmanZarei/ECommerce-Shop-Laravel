04129317f00ddb07c5563d7aa960db183a3866fc
ArmanZarei <armanzarei1378@gmail.com>
2021-09-30 16:03:14 +0330

    Admin Top Navbar

---
+++ resources/views/admin/partials/top_nav.blade.php
@@ -0,0 +1,55 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <div class="container-fluid ps-4 pe-4">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="nav" id="top-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Pages</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Apps</a>
+                </li>
+            </ul>
+            <div class="flex-shrink-0 dropdown ms-auto">
+                <a href="#" class="d-flex align-items-center link-dark text-decoration-none" id="user-dropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span id="welcome-text" class="me-2">Hi, <span>Arman</span></span>
+                    <span id="user-avatar-substitute">A</span>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end border-0" aria-labelledby="user-dropdown" id="user-dropdown-menu">
+                    <li class="user-menu-item">
+                        <a class="dropdown-item d-flex align-items-center" href="#">
+                            <i class="fad fa-user-edit color-green"></i>
+                            <div class="d-flex flex-column user-menu-item-text">
+                                <span>My Profile</span>
+                                <span>Account settings and more</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="user-menu-item">
+                        <a class="dropdown-item d-flex align-items-center" href="#">
+                            <i class="fad fa-cogs color-red"></i>
+                            <div class="d-flex flex-column user-menu-item-text">
+                                <span>My Messages</span>
+                                <span>Inbox and tasks</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="user-menu-item">
+                        <a class="dropdown-item d-flex align-items-center" href="#">
+                            <i class="fad fa-calendar-alt color-blue"></i>
+                            <div class="d-flex flex-column user-menu-item-text">
+                                <span>My Activities</span>
+                                <span>Logs and notifications</span>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
