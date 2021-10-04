afd7c5fbde35a251d30c06952e1feec499d1d91c
ArmanZarei <armanzarei1378@gmail.com>
2021-10-04 22:28:33 +0330

    Style & JS sections in master layout

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>eCommerce Shop - @yield('title')</title>
+
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/common/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common/fontawesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/admin/main.css') }}">
     @notifyCss
+    @yield('styles')
+    <link rel="stylesheet" href="{{ asset('css/admin/main.css') }}">
 </head>
 <body>
 
@@ -26,5 +28,8 @@
 
 <script src="{{ asset('js/common/bootstrap.bundle.min.js') }}"></script>
 @notifyJs
+<script src="{{ asset('js/common/main.js') }}"></script>
+@yield('scripts')
+
 </body>
 </html>
