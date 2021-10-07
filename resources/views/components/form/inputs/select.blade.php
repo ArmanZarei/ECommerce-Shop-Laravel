9b597ecd9eb02f6cd0457230102c5d5732c6b829
ArmanZarei <armanzarei1378@gmail.com>
2021-10-07 21:46:37 +0330

    Fix multiple bug

--- resources/views/components/form/inputs/select.blade.php
+++ resources/views/components/form/inputs/select.blade.php
@@ -1,9 +1,9 @@
-@props(['name', 'dataArray', 'multiselect' => false, 'dataValue' => 'id', 'dataName' => 'name', 'label' => null])
+@props(['name', 'dataArray', 'multiple' => false, 'dataValue' => 'id', 'dataName' => 'name', 'label' => null])
 @php($label = $label ?? ucwords($name))
 
 <div class="input-relative position-relative">
     <span class="custom-span-input-label">{{ $label }}</span>
-    <select class="selectpicker" data-live-search="true" name="{{ $name }}" {{ $multiselect ? 'multiple' : '' }}>
+    <select class="selectpicker" data-live-search="true" name="{{ $name }}" {{ $multiple ? 'multiple' : '' }}>
         @foreach($dataArray as $data)
             <option value="{{ $data->$dataValue }}" {{ $data->$dataValue == old($name) ? 'selected' : '' }}>{{ $data->$dataName }}</option>
         @endforeach
