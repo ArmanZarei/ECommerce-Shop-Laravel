790647784d75fdba4c3dea8f7a54851f1650c6f9
ArmanZarei <armanzarei1378@gmail.com>
2021-10-08 13:11:50 +0330

    pass other attributes to form component

--- resources/views/components/form/form.blade.php
+++ resources/views/components/form/form.blade.php
@@ -1,6 +1,6 @@
 @props(['action', 'method' => 'POST'])
 
-<form action="{{ $action }}" method="POST" class="{{ $attributes->class(['custom-form'])->get('class') }}">
+<form action="{{ $action }}" method="POST" class="{{ $attributes->class(['custom-form'])->get('class') }}" {{ $attributes->except('class') }}>
     @if(strtoupper($method) != 'POST')
         @method($method)
     @endif
