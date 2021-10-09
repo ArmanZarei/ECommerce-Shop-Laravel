7691793620ede3ff7be973e1c2296d46d55fbb78
ArmanZarei <armanzarei1378@gmail.com>
2021-10-09 22:46:57 +0330

    Add error to select input component

--- resources/views/components/form/inputs/select.blade.php
+++ resources/views/components/form/inputs/select.blade.php
@@ -1,7 +1,7 @@
 @props(['name', 'dataArray', 'multiple' => false, 'dataValue' => 'id', 'dataName' => 'name', 'label' => null])
 @php($label = $label ?? ucwords($name))
 
-<div class="input-relative position-relative">
+<div class="input-select-container position-relative {{ $errors->has($name) ? 'has-error' : '' }}">
     <span class="custom-span-input-label">{{ $label }}</span>
     <select class="selectpicker" data-live-search="true" name="{{ $name }}" {{ $multiple ? 'multiple' : '' }}>
         <option value="">Nothing selected</option>
@@ -9,4 +9,7 @@
             <option value="{{ $data->$dataValue }}" {{ $data->$dataValue == old($name) ? 'selected' : '' }}>{{ $data->$dataName }}</option>
         @endforeach
     </select>
+    @if($errors->has($name))
+        <p class="input-error">{{ $errors->first($name) }}</p>
+    @endif
 </div>
