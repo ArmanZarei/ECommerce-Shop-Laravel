3fcd5262c9a2e9c98835f766cec30a3cef5d4e9e
ArmanZarei <armanzarei1378@gmail.com>
2021-10-10 00:00:53 +0330

    Add default value to text input component

--- resources/views/components/form/inputs/text.blade.php
+++ resources/views/components/form/inputs/text.blade.php
@@ -1,9 +1,9 @@
-@props(['name', 'type' => 'text', 'label' => null])
+@props(['name', 'value' => null, 'type' => 'text', 'label' => null])
 @php($label = $label ?? ucwords($name))
 
 <div class="input-container">
     <div class="floating-label-container">
-        <input type="{{ $type }}" name="{{ $name }}" autocomplete="off" placeholder=" " class="{{ $errors->has($name) ? 'has-error' : '' }}" value="{{ old($name) }}">
+        <input type="{{ $type }}" name="{{ $name }}" autocomplete="off" placeholder=" " class="{{ $errors->has($name) ? 'has-error' : '' }}" value="{{ $value ?? old($name) }}">
         <label>{{ $label }}</label>
     </div>
     @if($errors->has($name))
