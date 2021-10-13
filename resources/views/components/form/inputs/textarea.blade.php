43ac9773446c43526f4f6320e26b5321027eda71
ArmanZarei <armanzarei1378@gmail.com>
2021-10-13 16:54:39 +0330

    Textarea component with default value

--- resources/views/components/form/inputs/textarea.blade.php
+++ resources/views/components/form/inputs/textarea.blade.php
@@ -1,9 +1,9 @@
-@props(['name', 'label' => null])
+@props(['name', 'label' => null, 'value' => null])
 @php($label = $label ?? ucwords($name))
 
 <div class="input-container">
     <div class="floating-label-container">
-        <textarea class="form-control" rows="3" placeholder=" " name="{{ $name }}">{{ old($name) }}</textarea>
+        <textarea class="form-control" rows="3" placeholder=" " name="{{ $name }}">{{ old($name) ?? $value }}</textarea>
         <label class="form-label">{{ $label }}</label>
     </div>
 </div>
