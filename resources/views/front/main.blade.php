8479e63fdeb4e577c45631e91848d34c75595e8d
ArmanZarei <armanzarei1378@gmail.com>
2021-11-26 22:41:27 +0330

    Fix products grid

--- resources/views/front/main.blade.php
+++ resources/views/front/main.blade.php
@@ -4,21 +4,25 @@
 
 @section('content')
     <div class="container mt-5">
-        @foreach($products as $product)
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="{{ $product->primaryImageUrl }}" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $product->name }}</h5>
-                    <p class="card-text">{{ \Illuminate\Support\Str::words($product->description, 10) }}</p>
+        <div class="row">
+            @foreach($products as $product)
+                <div class="col-3">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="{{ $product->primaryImageUrl }}" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $product->name }}</h5>
+                            <p class="card-text">{{ \Illuminate\Support\Str::words($product->description, 10) }}</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item d-flex justify-content-between"><span>Brand</span><span>{{ $product->brand->name }}</span></li>
+                            <li class="list-group-item d-flex justify-content-between"><span>Category</span><span>{{ $product->category->name }}</span></li>
+                        </ul>
+                        <div class="card-body">
+                            <a href="#" class="card-link btn btn-outline-primary">Details</a>
+                        </div>
+                    </div>
                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item d-flex justify-content-between"><span>Brand</span><span>{{ $product->brand->name }}</span></li>
-                    <li class="list-group-item d-flex justify-content-between"><span>Category</span><span>{{ $product->category->name }}</span></li>
-                </ul>
-                <div class="card-body">
-                    <a href="#" class="card-link btn btn-outline-primary">Details</a>
-                </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 @endsection
