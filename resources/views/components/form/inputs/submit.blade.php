7fbd1233ebc9632669fb458cc0027390ebc8c458
ArmanZarei <armanzarei1378@gmail.com>
2021-12-10 14:30:31 +0330

    Add class attribute to submit input component

--- resources/views/components/form/inputs/submit.blade.php
+++ resources/views/components/form/inputs/submit.blade.php
@@ -1,1 +1,1 @@
-<button class="btn btn-style-1 no-duration btn-light-primary mr-2">Submit</button>
+<button class="btn btn-style-1 no-duration btn-light-primary mr-2 {{ $attributes->get('class') }}">Submit</button>
