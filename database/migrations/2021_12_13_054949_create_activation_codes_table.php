192dabe922e0fc564567e7535df43fb6b9ef23ca
ArmanZarei <armanzarei1378@gmail.com>
2021-12-13 10:33:40 +0330

    Add activated at to users table

---
+++ database/migrations/2021_12_13_054949_create_activation_codes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivationCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activation_codes', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('code', 10);
+            $table->string('token');
+            $table->timestamp('expires_at');
+            $table->primary('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activation_codes');
+    }
+}
