4ab0f78e6ab17e59354a4bfc63767346ea43cc79
ArmanZarei <armanzarei1378@gmail.com>
2021-12-14 23:25:46 +0330

    Update textarea component

--- resources/views/components/form/inputs/textarea.blade.php
+++ resources/views/components/form/inputs/textarea.blade.php
@@ -1,9 +1,13 @@
 @props(['name', 'label' => null, 'value' => null])
 @php($label = $label ?? ucwords($name))
+@php($errorKey = $errorKey ?? $name)
 
 <div class="input-container">
     <div class="floating-label-container">
-        <textarea class="form-control" rows="3" placeholder=" " name="{{ $name }}">{{ old($name) ?? $value }}</textarea>
+        <textarea class="form-control {{ $errors->has($errorKey) ? 'has-error' : '' }}" rows="3" placeholder=" " name="{{ $name }}">{{ old($name) ?? $value }}</textarea>
         <label class="form-label">{{ $label }}</label>
     </div>
+    @if($errors->has($errorKey))
+        <p class="input-error">{{ $errors->first($errorKey) }}</p>
+    @endif
 </div>
