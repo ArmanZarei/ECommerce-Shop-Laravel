2f3008f60c87724e0b4ed0d9a0a79128deb640e1
ArmanZarei <armanzarei1378@gmail.com>
2021-12-15 13:26:27 +0330

    CommentRates in product page

--- app/Http/Controllers/Front/ProductController.php
+++ app/Http/Controllers/Front/ProductController.php
@@ -10,6 +10,15 @@ class ProductController extends Controller
 {
     public function show(Product $product)
     {
-        return view('front.products.show', compact('product'));
+        $commentsRate = $product
+            ->comments()
+            ->join('product_rates', function ($join) {
+                $join->on('product_rates.product_id', '=', 'comments.product_id')
+                    ->on('product_rates.user_id', '=', 'comments.user_id');
+            })
+            ->select('comments.id as cid', 'product_rates.rate as rate')
+            ->get()->pluck('rate', 'cid')->toArray();
+
+        return view('front.products.show', compact('product', 'commentsRate'));
     }
 }
